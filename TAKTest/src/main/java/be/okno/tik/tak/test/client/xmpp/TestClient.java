c585112992e9e03f7d6df59ecd562674524cda14
geraldo <geraldo@2406436b-9132-46b4-8e88-f4937dad9824>
2011-01-28 11:41:31 +0000

    initial import of test server and client

---
+++ TAKTest/src/main/java/be/okno/tik/tak/test/client/xmpp/TestClient.java
@@ -0,0 +1,117 @@
+/* 
+ * This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ */
+
+/* test client for subscribing to nodes and receiving payloads */
+
+package be.okno.tik.tak.test.client.xmpp;
+
+import java.util.Iterator;
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smackx.packet.DiscoverItems;
+import org.jivesoftware.smackx.pubsub.AccessModel;
+import org.jivesoftware.smackx.pubsub.CollectionNode;
+import org.jivesoftware.smackx.pubsub.ConfigureForm;
+import org.jivesoftware.smackx.pubsub.FormType;
+import org.jivesoftware.smackx.pubsub.ItemPublishEvent;
+import org.jivesoftware.smackx.pubsub.LeafNode;
+import org.jivesoftware.smackx.pubsub.PayloadItem;
+import org.jivesoftware.smackx.pubsub.PubSubManager;
+import org.jivesoftware.smackx.pubsub.PublishModel;
+import org.jivesoftware.smackx.pubsub.SimplePayload;
+import org.jivesoftware.smackx.pubsub.listener.ItemEventListener;
+
+public class TestClient {
+  public static String host = "walls.okno.be";
+  //String user = "admin@walls.okno.be";
+  //String password = "t1k_t6k";
+  public static String user = "geraldo@walls.okno.be";
+  public static String password = "test";
+  public static ConnectionConfiguration config;
+  public static XMPPConnection connection;
+  public static PubSubManager manager;
+
+	public static void main(String[] args) {
+		  connect();		
+		  //test pubsub
+		  getClocks();
+		  //subscribe to clock
+		  subscribe("clock1");
+	  }
+	   
+	  public static void connect(){ 
+	    config = new ConnectionConfiguration(host, 5222);
+	    //config.setSASLAuthenticationEnabled(true); //throws an error, set to true?
+	    config.setSelfSignedCertificateEnabled(true);
+	    connection = new XMPPConnection(config);
+	    try 
+	    {
+	      connection.connect();
+	      // we log in  
+	      connection.login(user, password);
+	      //connection.loginAnonymously();
+	      System.out.println("logged in as "+connection.getUser());
+	    } 
+	    catch (XMPPException e1) 
+	    {
+	      e1.printStackTrace();
+	      System.out.println("not connected");
+	    }
+	    manager = new PubSubManager(connection, "pubsub." + host);
+	  }
+
+	  //PubSub, will subscribe us to TAK, returns a list of clocks
+	  public static void getClocks()
+	  {      
+	    try {
+	      DiscoverItems items = manager.discoverNodes(null);
+	      Iterator<DiscoverItems.Item> it = items.getItems();
+
+	      while (it.hasNext()) {
+	        DiscoverItems.Item item = it.next();
+	        System.out.println("Clock found:"+item.getNode());
+
+	        //LeafNode node = (LeafNode)manager.getNode(item.getNode());
+	        //publishPayload(node, new Double(Math.random()*10000).intValue());
+	      }
+	    } 
+	    catch(XMPPException e1) {
+	      System.out.println("retrieving nodes failed:");
+	      e1.printStackTrace();
+	    }
+	  }
+
+	  //test method for accessing one node we know that it was created by smack
+	  public static void subscribe(String clockId)
+	  {		
+	    try 
+	    {
+	      LeafNode clock = (LeafNode) manager.getNode(clockId);
+	      ItemEventListener myEventHandler = new ItemEventListener<PayloadItem>() 
+	      {
+	        // responder function
+	        public void handlePublishedItems(ItemPublishEvent<PayloadItem> subNode) 
+	        {
+	        	System.out.println("new tik:"+subNode.getItems().toString());
+	        }
+	      };
+	      clock.addItemEventListener(myEventHandler);
+	      clock.subscribe(connection.getUser());
+	      System.out.println("subscribed to "+clockId);
+	    }
+	    catch (XMPPException e1)
+	    {
+	    	System.out.println("xmpp error: "+e1.getXMPPError());
+	    }
+	  }
+	}
---
+++ TAKTest/src/main/java/be/okno/tik/tak/test/server/TestServer.java
@@ -0,0 +1,170 @@
+/* 
+ * This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ */
+
+/* test server for creating pubsub nodes and publishing payloads to it */
+
+package be.okno.tik.tak.test.server;
+
+import java.util.Iterator;
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smackx.packet.DiscoverItems;
+import org.jivesoftware.smackx.pubsub.AccessModel;
+import org.jivesoftware.smackx.pubsub.CollectionNode;
+import org.jivesoftware.smackx.pubsub.ConfigureForm;
+import org.jivesoftware.smackx.pubsub.FormType;
+import org.jivesoftware.smackx.pubsub.ItemPublishEvent;
+import org.jivesoftware.smackx.pubsub.LeafNode;
+import org.jivesoftware.smackx.pubsub.PayloadItem;
+import org.jivesoftware.smackx.pubsub.PubSubManager;
+import org.jivesoftware.smackx.pubsub.PublishModel;
+import org.jivesoftware.smackx.pubsub.SimplePayload;
+import org.jivesoftware.smackx.pubsub.listener.ItemEventListener;
+
+public class TestServer {
+	private static String host = new String("walls.okno.be");
+	private static XMPPConnection con;
+	private static PubSubManager manager = null;
+
+	public static void main(String[] args) {
+		login();
+		manager = new PubSubManager(con, "pubsub." + host);
+		// deleteNode("clocks");
+		// CollectionNode node = createRootNode("clocks");
+		// CollectionNode node = getRootNode("clocks");
+		// addNode("clock1");
+		// addNode("clock2");
+		// addNode("clock3");
+		doNodes();
+
+		if (con.isConnected()) {
+			LeafNode node;
+			try {
+				node = (LeafNode) manager.getNode("clock1");
+				publishPayload(node, new Integer(123456).intValue());
+				publishPayload(node, new Integer(99999).intValue());
+				publishPayload(node, new Double(Math.random() * 10000).intValue());
+			} catch (XMPPException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void login() {
+		ConnectionConfiguration config = new ConnectionConfiguration(host, 5222);
+		// config.setSASLAuthenticationEnabled(true);
+		config.setSelfSignedCertificateEnabled(true);
+
+		con = new XMPPConnection(config);
+		try {
+			con.connect();
+			con.login("admin@" + host, "t1k_t6k");
+			//con.loginAnonymously();
+			System.out.println("Connected as:" + con.getUser());
+		} catch (XMPPException e1) {
+			System.out.println("Connection fails:");
+			e1.printStackTrace();
+		}
+	}
+
+	public static void deleteNode(String nodeId) {
+		// if connected to server
+		if (con.isConnected()) {
+			// delete supernode
+			try {
+				manager.deleteNode(nodeId);
+				System.out.println("Node deleted");
+			} catch (XMPPException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static CollectionNode getRootNode(String nodeId) {
+		CollectionNode myNode = null;
+		// if connected to server
+		if (con.isConnected()) {
+			// discover supernode
+			try {
+				// myNode = (LeafNode) manager.createNode(nodeId, form);
+				// System.out.println("Node Created");
+				myNode = (CollectionNode) manager.getNode(nodeId);
+				System.out.println("Node retrieved");
+			} catch (XMPPException e1) {
+				System.out.println("creating/retrieving node fails:");
+				e1.printStackTrace();
+			}
+		}
+		return myNode;
+	}
+
+	public static void doNodes() {
+		try {
+			DiscoverItems items = manager.discoverNodes(null);
+			Iterator<DiscoverItems.Item> it = items.getItems();
+
+			while (it.hasNext()) {
+				DiscoverItems.Item item = it.next();
+				System.out.println("Node found:" + item.getNode());
+			}
+		} catch (XMPPException e1) {
+			System.out.println("retrieving nodes failed:");
+			e1.printStackTrace();
+		}
+	}
+
+	public static void addNode(String nodeId) {
+		// if connected to server
+		if (con.isConnected()) {
+			// Create and publish
+			ConfigureForm form = new ConfigureForm(FormType.submit);
+			form.setPersistentItems(false);
+			form.setSubscribe(true);
+			form.setDeliverPayloads(true);
+			form.setAccessModel(AccessModel.open);
+			form.setPublishModel(PublishModel.open);
+
+			// add node
+			try {
+				LeafNode myNode = (LeafNode) manager.createNode(nodeId, form);
+				System.out.println("node created: " + nodeId);
+			} catch (XMPPException e1) {
+				System.out.println("creating/retrieving node fails:");
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	public static void publishPayload(LeafNode node, int tiks) {
+		// create payload
+		// or better JSON? "{'id':1,'tiks':12345}"
+		SimplePayload payload = new SimplePayload("clock", "pubsub:clock",
+				"<clock xmlns='pubsub:clock'><id>" + node.getId() + "</id><tiks>" + tiks
+						+ "</tiks></clock>");
+
+		PayloadItem<SimplePayload> item = new PayloadItem<SimplePayload>(node.getId(), payload);
+
+		ItemEventListener myEventHandler = new ItemEventListener() {
+			public void handlePublishedItems(ItemPublishEvent items) {
+				if (items != null) {
+					System.out.println("resulting payload:" + items.getItems().toString());
+				}
+			}
+		};
+		node.addItemEventListener(myEventHandler);
+
+		// publish items
+		node.publish(item);
+		System.out.println("TIK item " + tiks + " published to node " + node.getId());
+	}
+}
